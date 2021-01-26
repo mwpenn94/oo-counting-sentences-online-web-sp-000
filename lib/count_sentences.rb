ded9e58f1c7f408c08ac94677b23159a094dc768
Michael Penn <mwpenn94@gmail.com>
2021-01-26 20:29:46 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentence = []
+    sentence_finder = self.split(/\.|\?|\![^a-z]|$/)
+		sentence_finder.each do |string|
+			 if string.size > 1
+				sentence << string
+			 end
+		end
+		sentence.count
   end
 end
